70dab63546a0d17c1b85840c9cbe85cf1a526be3
Carlo Gandolfo <carlo@artilibere.com>
2019-04-21 00:50:20 +0200

    add Purchare triple discount

---
+++ purchase_triple_discount-10.0.1.1.0/purchase_discount/models/purchase_order.py
@@ -0,0 +1,111 @@
+# -*- coding: utf-8 -*-
+# Copyright 2004-2009 Tiny SPRL (<http://tiny.be>).
+# Copyright 2016 ACSONE SA/NV (<http://acsone.eu>)
+# Copyright 2015-2017 Tecnativa - Pedro M. Baeza
+# Copyright 2018 Opener B.V. - Stefan Rijnhart
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import api, fields, models
+import odoo.addons.decimal_precision as dp
+from odoo.tools import frozendict
+
+
+class PurchaseOrder(models.Model):
+    _inherit = "purchase.order"
+
+    @api.depends('order_line.price_total')
+    def _amount_all(self):
+        """In the case that taxes rounding is set to globally, Odoo requires
+        again the line price unit, and currently ORM mixes values, so the only
+        way to get a proper value is to overwrite that part, losing
+        inheritability.
+        """
+        orders2recalculate = self.filtered(lambda x: (
+            x.company_id.tax_calculation_rounding_method ==
+            'round_globally' and any(x.mapped('order_line.discount'))
+        ))
+        super(PurchaseOrder, self)._amount_all()
+        for order in orders2recalculate:
+            amount_tax = 0
+            for line in order.order_line:
+                taxes = line.taxes_id.compute_all(
+                    line._get_discounted_price_unit(),
+                    line.order_id.currency_id,
+                    line.product_qty,
+                    product=line.product_id,
+                    partner=line.order_id.partner_id,
+                )
+                amount_tax += sum(
+                    t.get('amount', 0.0) for t in taxes.get('taxes', [])
+                )
+            order.update({
+                'amount_tax': order.currency_id.round(amount_tax),
+                'amount_total': order.amount_untaxed + amount_tax,
+            })
+
+
+class PurchaseOrderLine(models.Model):
+    _inherit = "purchase.order.line"
+
+    @api.depends('discount')
+    def _compute_amount(self):
+        """ Inject the product price with proper rounding in the context from
+        which account.tax::compute_all() is able to retrieve it. The alternate
+        context is patched onto self because it can be a NewId passed in the
+        onchange the env of which does not support `with_context`. """
+        for line in self:
+            orig_context = None
+            # This is always executed for allowing other modules to use this
+            # with different conditions than discount != 0
+            discounted_price_unit = line._get_discounted_price_unit()
+            if discounted_price_unit != line.price_unit:
+                precision = line.order_id.currency_id.decimal_places
+                company = line.company_id or self.env.user.company_id
+                if company.tax_calculation_rounding_method == 'round_globally':
+                    precision += 5
+                orig_context = self.env.context
+                price = round(
+                    line.product_qty * discounted_price_unit, precision)
+                self.env.context = frozendict(
+                    self.env.context, base_values=(price, price, price))
+            super(PurchaseOrderLine, line)._compute_amount()
+            if orig_context is not None:
+                self.env.context = orig_context
+
+    discount = fields.Float(
+        string='Discount (%)', digits=dp.get_precision('Discount'),
+    )
+
+    _sql_constraints = [
+        ('discount_limit', 'CHECK (discount <= 100.0)',
+         'Discount must be lower than 100%.'),
+    ]
+
+    def _get_discounted_price_unit(self):
+        """Inheritable method for getting the unit price after applying
+        discount(s).
+
+        :rtype: float
+        :return: Unit price after discount(s).
+        """
+        self.ensure_one()
+        if self.discount:
+            return self.price_unit * (1 - self.discount / 100)
+        return self.price_unit
+
+    @api.multi
+    def _get_stock_move_price_unit(self):
+        """Get correct price with discount replacing current price_unit
+        value before calling super and restoring it later for assuring
+        maximum inheritability.
+        """
+        price_unit = False
+        price = self._get_discounted_price_unit()
+        if price != self.price_unit:
+            # Only change value if it's different
+            price_unit = self.price_unit
+            self.price_unit = price
+        price = super(PurchaseOrderLine, self)._get_stock_move_price_unit()
+        if price_unit:
+            self.price_unit = price_unit
+        return price
---
+++ purchase_triple_discount-10.0.1.1.0/purchase_discount/tests/test_purchase_discount.py
@@ -0,0 +1,120 @@
+# -*- coding: utf-8 -*-
+# Copyright 2016 ACSONE SA/NV (<http://acsone.eu>)
+# Copyright 2015-2018 Tecnativa - Pedro M. Baeza
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+import odoo.tests.common as common
+from odoo import fields
+
+
+class TestPurchaseOrder(common.HttpCase):
+    """ HttpCase as a workaround for problems with wkhtmltopdf """
+    at_install = False
+    post_install = True
+
+    def setUp(self):
+        super(TestPurchaseOrder, self).setUp()
+        self.company = self.env.user.company_id
+        self.company.tax_calculation_rounding_method = 'round_per_line'
+        self.product_1 = self.env['product.product'].create({
+            'name': 'Test product 1',
+        })
+        self.product_2 = self.env['product.product'].create({
+            'name': 'Test product 2',
+        })
+        po_model = self.env['purchase.order.line']
+        # Make sure currency is EUR for not having troubles with rates
+        self.env.cr.execute(
+            "UPDATE res_company SET currency_id = %s WHERE id = %s",
+            (self.env.ref('base.EUR').id, self.env.user.company_id.id))
+        self.env.user.company_id.refresh()
+        self.purchase_order = self.env['purchase.order'].create({
+            'partner_id': self.env.ref('base.res_partner_3').id,
+        })
+        self.po_line_1 = po_model.create({
+            'order_id': self.purchase_order.id,
+            'product_id': self.product_1.id,
+            'date_planned': fields.Datetime.now(),
+            'name': 'Test',
+            'product_qty': 1.0,
+            'product_uom': self.product_1.uom_id.id,
+            'discount': 50.0,
+            'price_unit': 10.0,
+        })
+        self.tax = self.env['account.tax'].create({
+            'name': 'Sample tax 15%',
+            'amount_type': 'percent',
+            'type_tax_use': 'purchase',
+            'amount': 15.0,
+        })
+        self.po_line_2 = po_model.create({
+            'order_id': self.purchase_order.id,
+            'product_id': self.product_2.id,
+            'date_planned': fields.Datetime.now(),
+            'name': 'Test',
+            'product_qty': 10.0,
+            'product_uom': self.product_2.uom_id.id,
+            'discount': 30,
+            'taxes_id': [(6, 0, [self.tax.id])],
+            'price_unit': 230.0,
+        })
+        self.po_line_3 = po_model.create({
+            'order_id': self.purchase_order.id,
+            'product_id': self.product_2.id,
+            'date_planned': fields.Datetime.now(),
+            'name': 'Test',
+            'product_qty': 1.0,
+            'product_uom': self.product_2.uom_id.id,
+            'discount': 0,
+            'taxes_id': [(6, 0, [self.tax.id])],
+            'price_unit': 10.0,
+        })
+
+    def test_purchase_order_vals(self):
+        self.assertEqual(self.po_line_1.price_subtotal, 5.0)
+        self.assertEqual(self.po_line_2.price_subtotal, 1610.0)
+        self.assertEqual(self.po_line_3.price_subtotal, 10.0)
+        self.assertEqual(self.purchase_order.amount_untaxed, 1625.0)
+        self.assertEqual(self.purchase_order.amount_tax, 243)
+
+    def test_move_price_unit(self):
+        self.purchase_order.button_confirm()
+        moves = self.purchase_order.picking_ids.move_lines
+        self.assertEqual(moves[0].price_unit, 5,)
+        self.assertEqual(moves[1].price_unit, 161)
+        self.assertEqual(moves[2].price_unit, 10)
+        # Change price to launch a recalculation of totals
+        self.po_line_1.discount = 60
+        self.assertEqual(self.po_line_1.price_subtotal, 4.0)
+        self.assertEqual(self.purchase_order.amount_untaxed, 1624.0)
+        self.assertEqual(self.purchase_order.amount_tax, 243)
+
+    def test_report_price_unit(self):
+        rec = self.env['purchase.report'].search([
+            ('product_id', '=', self.product_1.id),
+        ])
+        self.assertEqual(rec.price_total, 5)
+        self.assertEqual(rec.discount, 50)
+
+    def test_rounding(self):
+        """ Check that the unit price is not rounded after discount is applied
+        as this can lead to differences with the created invoice. """
+        po_line = self.env['purchase.order.line'].new({
+            'order_id': self.purchase_order.id,
+            'product_id': self.product_2.id,
+            'date_planned': fields.Datetime.now(),
+            'name': 'Test',
+            'product_qty': 195.0,
+            'product_uom': self.product_2.uom_id.id,
+            'discount': 7,
+            'taxes_id': [],
+            'price_unit': 14.49,
+        })
+        po_line._compute_amount()
+        self.assertEqual(po_line.price_subtotal, 2627.76)
+
+
+class TestPurchaseOrderRoundGlobally(TestPurchaseOrder):
+    def setUp(self):
+        super(TestPurchaseOrderRoundGlobally, self).setUp()
+        self.company.tax_calculation_rounding_method = 'round_globally'
